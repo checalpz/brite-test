42b932454d5872936df83642d789579255d627fd
checalpz <checalpz@gmail.com>
2024-10-26 14:30:40 +0200

    Added tests for /v2/berry/{id or name} endpoint calls

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.imdb.com',
+    baseUrl: 'https://www.imdb.com',    //default
     videoUploadOnPasses: false
   },
+  env: {
+    apiBaseUrl : 'https://pokeapi.co'
+
+  }
 });
