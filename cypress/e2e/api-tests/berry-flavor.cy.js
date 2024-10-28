f2dd42974641e94daf686b54fc9a14feb8df9e35
checalpz <checalpz@gmail.com>
2024-10-28 22:54:20 +0100

    Improvements in the API tests: Added new comments

--- cypress/e2e/api-tests/berry-flavor.cy.js
+++ cypress/e2e/api-tests/berry-flavor.cy.js
@@ -1,3 +1,8 @@
+const berryFlavor = 'spicy'
+
+/*
+    Given a array of berries, return the information of the berry with more potency
+*/
 function morePotency(berries) {
     let potency = 0
     let resultBerry = null
@@ -12,14 +17,16 @@ function morePotency(berries) {
     return resultBerry
 }
 
-function getPotencyByFlavor(respBody, flavorName){
-    
+/*
+    Return the  potency of a given flavor searching in the response body of the endpoint,
+*/
+function getPotencyByFlavor(respBody, flavorName) {
+
     const flavorPotency = respBody.find((flavor) => flavor.flavor.name === flavorName);
-    
+
     return flavorPotency.potency;
 }
 
-const berryFlavor = 'spicy'
 
 describe('Spicy flavour API', () => {
 
@@ -29,33 +36,32 @@ describe('Spicy flavour API', () => {
     })
 
     it('Berry flavour', () => {
+
+        // Call the first endpoint, confirm if all the necesary field  exist
         cy.api({
             method: 'GET',
             url: `/api/v2/berry-flavor/${berryFlavor}`
         }).then((resp) => {
             expect(resp.status).to.eq(200)
-            
-            cy.log(resp.body['berries'])
+            expect(resp.body['berries']).to.exist
 
             const morePotencyBerry = morePotency(resp.body['berries'])
             const berryName = morePotencyBerry['berry']['name']
             const berryPotency = morePotencyBerry['potency']
-            const berryId = parseInt(morePotencyBerry['berry']['url'].split('/').at(-2))
+            const berryId = parseInt(morePotencyBerry['berry']['url'].split('/').at(-2))    // Split the URL to get the berry id
 
             cy.api({
                 method: 'GET',
                 url: `/api/v2/berry/${berryName}`
             }).then((resp) => {
                 expect(resp.status).to.eq(200)
-                
+
+                // Compare that information receive in both call are the same
                 expect(resp.body['name']).to.eq(berryName)
                 expect(resp.body['id']).to.eq(berryId)
                 expect(getPotencyByFlavor(resp.body['flavors'], berryFlavor)).to.eq(berryPotency)
 
             })
         })
-
-
     })
-
 })
--- cypress/e2e/api-tests/berry.cy.js
+++ cypress/e2e/api-tests/berry.cy.js
@@ -8,6 +8,7 @@ describe('Poke API', () => {
     })
 
     parameters.forEach((parameter) => {
+        // Happy path - Validate call the endpoint with a valid parameter (id and/or name)
         it(`OK with a valid parameter (${parameter})`, () => {
             cy.api({
                 method: 'GET',
@@ -17,16 +18,15 @@ describe('Poke API', () => {
             })
         })
 
+        // Unhappy path - Validate error when the calling with a invalid parameter (id and/or name)
         it(`Error code for invalid parameter (${parameter})`, () => {
             cy.api({
                 method: 'GET',
                 url: `/api/v2/berry/${parameter}-invalid`,
-                failOnStatusCode: false         // To advoid default failed
+                failOnStatusCode: false         // To advoid default failed (It's expected that the call fail)
             }).then((resp) => {
                 expect(resp.status).to.eq(404)
             })
         })
-
-
     })
 })
